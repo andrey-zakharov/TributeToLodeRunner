413096d905d7daf5d8fd90f27ff79e420b32f974
Andrey Zakharov <andrey.al.zakharov@gmail.com>
2022-10-27 00:43:35 +0300

    Saving draft

--- src/jvmMain/pixelHunter.kt
+++ src/jvmMain/pixelHunter.kt
@@ -2,6 +2,8 @@ package me.az
 
 import java.io.File
 import java.io.InputStream
+import java.lang.Exception
+import java.nio.ByteBuffer
 import kotlin.math.*
 import kotlin.reflect.typeOf
 import kotlin.system.exitProcess
@@ -19,8 +21,8 @@ fun Int.factors(k: Int = 2): Map<Int, Int> {
         r /= k
     }
     val o = r.factors( if ( k < 3 ) k + 1 else k + 2)
-    o.forEach { k, d ->
-        res.put( k, res.getOrDefault(k, 0) + d )
+    o.forEach { ok, d ->
+        res.put( ok, res.getOrDefault(ok, 0) + d )
     }
     return res
 }
@@ -39,13 +41,12 @@ val palette = arrayOf(
     '▮', '▮', '▮', '▮', '▮', '▮', '▮', '▮',
 )
 // or use colors for 1
-fun pixelsStream(
-    inputStream: InputStream
-    bitsPerPixel: Int = 2,
-    limitRows: Int = Int.MAX_VALUE,
-    skipEmptyRows: Boolean = false,
-    export: Boolean = false
-) = sequence<Int> {
+fun InputStream.pixelsStream(
+    bytesWindow: Int = 32,
+    bitsPerPixel: Int = 2
+) = sequence<UInt> {
+
+    val windowBuff = ByteArray(bytesWindow)
     val mask = (2f.pow( bitsPerPixel ) - 1).toUInt()
     var currentByte = Int.MAX_VALUE
     var currentShift = 0
@@ -55,7 +56,7 @@ fun pixelsStream(
     while( red > 0 ) {
         if ( currentByte >= bytesWindow ) {
             //read  next chunk
-            red = reader.read(windowBuff)
+            red = read(windowBuff)
 //                println("WINDOW READ= ${windowBuff.joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } }")
             currentByte = 0
         }
@@ -79,6 +80,67 @@ fun pixelsStream(
         }
     }
 }
+
+fun InputStream.readAll(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: Int = 11) {
+    var p =0
+    val markBuf = ByteArray(mark.size)
+    while( available() > 0 ) {
+        read(markBuf)
+        if (markBuf[0] == mark[0] && markBuf[1] == mark[1]) {
+            p ++
+            // mark[0] was rows, mark[1] bytes per row (= 3 * 8 / 2  = 12)
+//            println(pos.toString(16).padStart(8, '0'))
+//            printSprite(bitsPerPixel, width, height)
+//            println(read().toString(16))
+//            println(read().toString(16))
+//            println(read().toString(16))
+//            println(read().toString(16))
+            println()
+        }
+    }
+    println("total $p")
+}
+
+// returns pixels red
+fun InputStream.printSprite(
+    bitsPerPixel: Int = 2,
+    width: Int = 12,
+    height: Int = 11,
+    skipEmptyRows: Boolean = false,
+    export: Boolean = false,
+): Int {
+    val bytesWindow = 8.lcm( bitsPerPixel * width ) / 8
+//    println("window size in bytes = $bytesWindow")
+
+    val stream = pixelsStream(
+        bytesWindow = bytesWindow,
+        bitsPerPixel = bitsPerPixel
+    )
+
+    // BYTE ORDER????
+    var printedRows = 0
+    var redPixels = 0
+
+    stream.takeWhile { printedRows < height }.chunked(width).forEach { row ->
+        val rowStr = row.map {
+            if ( export ) "${it.toString(10)}"
+            else
+                if ( it == 0u ) " "
+                else "\u001b[38;5;${it % 255u}m${palette[1]}"
+        }.joinToString("")
+
+        redPixels += row.size
+
+        if ( rowStr.trim().isEmpty() && skipEmptyRows ) {
+            return@forEach
+        }
+
+        printedRows ++
+        println("$rowStr\u001b[0m")
+    }
+    return redPixels
+}
+
 fun main(args: Array<String>) {
 //    println(4.lcm(3))
 //    println(18.lcm(8))
@@ -88,81 +150,27 @@ fun main(args: Array<String>) {
         exitProcess(-1)
     }
 
-    val rowWidth = if ( args.size > 1) Integer.decode(args[1]) else 32 // in "pixels"
+    val f = File(args[0].toString())
+    val reader = f.inputStream()
+    if ( args.size > 1 && args[1] == "mark" ) {
+        val mark = ByteArray(2) { listOf(0x0b, 0x03)[it].toByte() }
+        reader.readAll(mark)
+        exitProcess(0)
+    }
+
+    val rowWidth = try { Integer.decode(args[1]) } catch (e: Exception) {32} // in "pixels"
     val bitsPerPixel = if ( args.size > 2) Integer.decode(args[2]) else 4 //
     val offset = if ( args.size > 3 ) Integer.decode(args[3]) else 0
     val limitRows = if ( args.size > 4 ) Integer.decode(args[4]) else Int.MAX_VALUE
     val skipEmptyRows = if ( args.size > 5 ) args[5].toBoolean() else false
     val export = if ( args.size > 6 ) args[6].toBoolean() else false
 
-val f = File(args[0].toString())
-val reader = f.inputStream()
-reader.skip(offset.toLong())
-
+    reader.skip(offset.toLong())
 //println(f.readBytes().joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } )
 
-val bytesWindow = 8.lcm( bitsPerPixel * rowWidth ) / 8
-println("window size in bytes = $bytesWindow")
-val windowBuff = ByteArray(bytesWindow)
-
-    val pixelsStream = sequence<UInt> {
-        val mask = (2f.pow( bitsPerPixel ) - 1).toUInt()
-        var currentByte = Int.MAX_VALUE
-        var currentShift = 0
-        var currentMask = (mask shl (8 - bitsPerPixel)).toUByte()
-        var red = Int.MAX_VALUE
-
-        while( red > 0 ) {
-            if ( currentByte >= bytesWindow ) {
-                //read  next chunk
-                red = reader.read(windowBuff)
-//                println("WINDOW READ= ${windowBuff.joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } }")
-                currentByte = 0
-            }
-
-            val f = windowBuff[currentByte].toUByte() and currentMask
-            val res = f.toInt() shr (8 - currentShift - bitsPerPixel)
-//
-//            println("currentByte=$currentByte code=${windowBuff[currentByte].toUByte().toString(16)} " +
-//                    "\ncode=${windowBuff[currentByte].toUByte().toString(2).padStart(8, '0')}" +
-//                    "\nmask = ${currentMask.toString(2).padStart(8, '0')} " +
-//                    "\tres = $res")
-//
-            yield( res.toUInt() )
-//
-            currentMask = (currentMask.toInt() shr bitsPerPixel).toUByte()
-            currentShift += bitsPerPixel
-            if ( currentShift >= 8 ) { // byte border TBD for 3 bits per pixel!
-                ++currentByte
-                currentShift = 0
-                currentMask = (mask shl (8 - bitsPerPixel)).toUByte()
-            }
-
-        }
-    }
-
-        // BYTE ORDER????
-        var printedRows = 0
-        var redPixels = 0
-
-        pixelsStream.takeWhile { printedRows < limitRows }.chunked(rowWidth).forEach { row ->
-            val rowStr = row.map {
-                if ( export ) "${it.toString(10)}"
-                else
-                    if ( it == 0u ) " "
-                    else "\u001b[38;5;${it % 255u}m${palette[1]}"
-            }.joinToString("")
-
-            redPixels += row.size
-
-            if ( rowStr.trim().isEmpty() && skipEmptyRows ) {
-                return@forEach
-            }
-
-            printedRows ++
-            println("$rowStr\u001b[0m")
-        }
-
+    val redPixels = reader.printSprite(
+        bitsPerPixel, rowWidth, limitRows, skipEmptyRows, export
+    )
     println("new offset = 0x${(offset + (redPixels * bitsPerPixel / 8)).toString(16)}")
 
     exitProcess(0)
