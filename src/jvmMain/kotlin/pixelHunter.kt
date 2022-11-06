597f4110b7a0207e1a893f4fe819cd060e0b1fea
Andrey Zakharov <andrey.al.zakharov@gmail.com>
2022-11-06 20:14:40 +0300

    Many bpp support

--- src/jvmMain/kotlin/pixelHunter.kt
+++ src/jvmMain/kotlin/pixelHunter.kt
@@ -2,17 +2,34 @@ package me.az
 // sudo aptitude install coreutils
 import java.io.File
 import java.io.InputStream
+import java.io.PrintWriter
 import java.lang.Exception
-import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import kotlin.math.*
-import kotlin.reflect.typeOf
 import kotlin.system.exitProcess
 
-val headerHeight = 1
-val footerHeight = 1
-val columnsPad = 3
+const val headerHeight = 1
+const val footerHeight = 2
+const val columnsPad = 3
+
+const val lblColor = 14
+const val valColor = 2
+var verbose = false
 // kotlinc pixelHunter.kts -include-runtime -d pixelHunter.jar
+
+class AppContext {
+    val calcColCount get() = (termWidth + columnsPad) / (rowWidth + columnsPad)
+
+    var termWidth = 0 // cache
+    var offset = 0 // live var
+    var bitsPerPixel = 4
+    var rowWidth = 32
+    var limitRows = Int.MAX_VALUE // but how many for export? to take snapshots....
+    // ruler
+    var skipEmptyRows = false
+    var export = false
+    var exit = false
+}
 
 // factor and its degree
 fun Int.factors(k: Int = 2): Map<Int, Int> {
@@ -40,14 +57,10 @@ fun Int.lcm(o: Int): Int {
 
 val Map<Int, Int>.factor get() = entries.fold(1) { r, e -> r * e.key.toFloat().pow( e.value ).toInt() }
 
-val palette = arrayOf(
-    ' ', '▮', '▯', '▼', '▽', '◆', '◇', '◈',
-    '▮', '▮', '▮', '▮', '▮', '▮', '▮', '▮',
-)
-@kotlin.ExperimentalUnsignedTypes
+@ExperimentalUnsignedTypes
 fun List<UByte>.pixelStream(
     bitsPerPixel: Int = 2
-) = sequence<Int> {
+) = sequence {
 
     val mask = (2f.pow( bitsPerPixel ) - 1).toUInt()
     var currentByte = 0
@@ -73,138 +86,155 @@ fun List<UByte>.pixelStream(
 fun InputStream.pixelsStream(
     bytesWindow: Int = 32,
     bitsPerPixel: Int = 2
-) = sequence<Int> {
-
+) = sequence {
     val windowBuff = ByteArray(bytesWindow)
-    val mask = (2f.pow( bitsPerPixel ) - 1).toUInt()
+    val mask = (2f.pow( bitsPerPixel ) - 1).toInt()
+    if ( verbose ) println("mask = ${mask.toString(2).padStart(8, '0')} ")
     var currentByte = Int.MAX_VALUE
-    var currentShift = 0
-    var currentMask = (mask shl (8 - bitsPerPixel)).toUByte()
+    var currentBit = 0
     var red = Int.MAX_VALUE
+    var longRes = 0
+    var resPos = 0
 
     while( red > 0 ) {
+
         if ( currentByte >= bytesWindow ) {
             //read  next chunk
             red = read(windowBuff)
-//                println("WINDOW READ= ${windowBuff.joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } }")
+            if ( red <= 0 ) break
+            if ( verbose ) println("$bytesWindow WINDOW READ= ${windowBuff.joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } }")
             currentByte = 0
         }
 
-        val f = windowBuff[currentByte].toUByte() and currentMask
-        val res = f.toInt() shr (8 - currentShift - bitsPerPixel)
+        val curByte = windowBuff[currentByte].toInt()
+        val bitsTake = min(8 - currentBit, bitsPerPixel - resPos )
+        val rightShift = 8 - (currentBit + bitsTake)
+        // normalize
+        val normalizedPart = (curByte ushr rightShift) and (2f.pow( bitsTake ) - 1).toInt()
+        val leftShift = bitsPerPixel - resPos - bitsTake
+        val resPart = normalizedPart shl leftShift
+        longRes = longRes or resPart
+        resPos += bitsTake
+        //
+        if ( verbose ) {
+            println(
+                "bytePos=$currentByte bitPos=$currentBit code=${windowBuff[currentByte].toUByte().toString(16)} " +
+                " ${windowBuff[currentByte].toUByte().toString(2).padStart(8, '0')}" +
+                "\tres = $longRes"
+            )
+        }
+
+        if ( resPos >= bitsPerPixel ) {
+            yield(longRes)
+            longRes = 0
+            resPos = 0
+        }
 //
-//            println("currentByte=$currentByte code=${windowBuff[currentByte].toUByte().toString(16)} " +
-//                    "\ncode=${windowBuff[currentByte].toUByte().toString(2).padStart(8, '0')}" +
-//                    "\nmask = ${currentMask.toString(2).padStart(8, '0')} " +
-//                    "\tres = $res")
-//
-        yield( res )
-//
-        currentMask = (currentMask.toInt() shr bitsPerPixel).toUByte()
-        currentShift += bitsPerPixel
-        if ( currentShift >= 8 ) { // byte border TBD for 3 bits per pixel!
-            ++currentByte
-            currentShift = 0
-            currentMask = (mask shl (8 - bitsPerPixel)).toUByte()
+        currentBit += bitsTake
+        if ( currentBit >= 8 ) {
+            currentByte ++
+            currentBit = 0
+        }
+    }
+}
+
+sealed class SpritePrinter {
+    abstract var printedRows: Int
+    abstract var redPixels: Int
+    abstract fun visitRow(y: Int, row: List<Int>)
+
+    class TermPrinter(
+        private val term: Term,
+        private val skipEmptyRows: Boolean = false,
+        private val col: Int = 0) : SpritePrinter() {
+
+        override var printedRows = 0
+        override var redPixels = 0
+
+        override fun visitRow(y: Int, row: List<Int>) {
+            val rowStr = row.map {
+                if ( it == 0 ) "\u001b[0m "
+                else "\u001b[48;5;${it % 255}m "
+            }.joinToString("")
+
+            redPixels += row.size
+
+            if ( rowStr.trim().isEmpty() && skipEmptyRows ) {
+                return
+            }
+
+            if ( col > 0 ) term.move(col, printedRows + headerHeight)
+            println("$rowStr\u001b[0m")
+            printedRows ++
+        }
+    }
+
+    class ImageMagickExporter(private val out: PrintWriter, width: Int, height: Int,
+                              private val palette: Map<Int, String>): SpritePrinter() {
+        override var printedRows: Int = 0
+        override var redPixels: Int = 0
+
+        init {
+            out.println("# ImageMagick pixel enumeration: $width,$height,255,rgb")
+        }
+
+        override fun visitRow(y: Int, row: List<Int>) {
+            row.forEachIndexed { x, p ->
+                out.println("$x,$y: (${palette[p]})")
+            }
+            redPixels + row.size
+            printedRows ++
         }
     }
 }
 
 // returns pixels red
 fun InputStream.printSprite(
+    printer: SpritePrinter,
     bitsPerPixel: Int = 2,
     width: Int = 12,
     height: Int = 11,
-    skipEmptyRows: Boolean = false,
-    export: Boolean = false,
-    term: Term? = null, col: Int = 0,
 ): Int {
-    val bytesWindow = 8.lcm( bitsPerPixel * width ) / 8
-//    println("window size in bytes = $bytesWindow")
-
     val stream = pixelsStream(
-        bytesWindow = bytesWindow,
+        bytesWindow = 8.lcm( bitsPerPixel * width ) / 8,
         bitsPerPixel = bitsPerPixel
     )
 
     // BYTE ORDER????
-    var printedRows = 0
-    var redPixels = 0
 
-    stream.takeWhile { printedRows < height }.chunked(width).forEach { row ->
-        val rowStr = row.map {
-            it.printPixel(export)
-        }.joinToString("")
-
-        redPixels += row.size
-
-        if ( rowStr.trim().isEmpty() && skipEmptyRows ) {
-            return@forEach
-        }
-
-        if ( col > 0 ) term?.move(col, printedRows + me.az.headerHeight)
-        println("$rowStr\u001b[0m")
-        printedRows ++
+    stream.takeWhile { printer.printedRows < height }.chunked(width).forEachIndexed { y, row ->
+        printer.visitRow(y, row)
     }
-    return redPixels
+    return printer.redPixels
 }
-
-fun Int.printPixel(export: Boolean = false): String =
-    if ( export ) "${toString(10)}"
-    else
-        if ( this == 0 ) "\u001b[0m "
-        // \u001b[47m
-//        else "\u001b[38;5;${this % 255}m${palette[1]}"
-        else "\u001b[48;5;${this % 255}m "
-
-fun InputStream.readAll(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: Int = 11) {
-    var p = 0
-    var curMarkI = 0
-    val charBuff = ByteArray(1)
-
-    while( available() > 0 ) {
-
-        while ( curMarkI < mark.size && read(charBuff) > 0 && mark[curMarkI] == charBuff[0] ) {
-            curMarkI++
-        }
-        if ( curMarkI == mark.size ) {// exact match
-            val red = printSprite(bitsPerPixel, width, height)
-            println("red = $red bytes")
-            p++
-
-        } else {
-            // failed match
-        }
-        curMarkI = 0
-    }
-
-    println("total $p")
-}
-
-val cga = mapOf(
-    0 to "0, 0, 0",
-    1 to "0, 170, 170",
-    2 to "170, 0, 170",
-    3 to "170, 170, 170",
+val palettes = mapOf(
+    BitsPerPixel.ONE.bits to mapOf(
+        0 to "0, 0, 0",
+        1 to "255, 255, 255"
+    ),
+    BitsPerPixel.FOUR.bits to mapOf(
+        0 to "0, 0, 0",
+        1 to "0, 170, 170",
+        2 to "170, 0, 170",
+        3 to "170, 170, 170",
+    )
 )
 
-@kotlin.ExperimentalUnsignedTypes
-fun File.parse(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: Int = 11, export: Boolean = false) {
+@ExperimentalUnsignedTypes
+fun File.dumpAppleImages(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: Int = 11, export: Boolean = false) {
 
+    // apple dump
     var p = 0
     val b = readBytes().asUByteArray()
     val m1 = 0x0b.toUByte()
     val m2 = 0x03.toUByte()
-
-
-
 
     for ( i in 0 until b.size - 1 ) {
         if (b[i] == m1 && b[i+1] == m2) {
             val px = b.slice( i + 2 until i + 2 + (width * height * bitsPerPixel / 8) )
                 .pixelStream(bitsPerPixel)
 
-            val channels = Array(cga.size) { mutableListOf<Pair<Int, Int>>() }
+//            val channels = Array(cga.size) { mutableListOf<Pair<Int, Int>>() }
 
 //            val resFile = File("${(i + 2).toString(16)}.png")
             val resFile = File("${(i + 2).toString(16)}.txt")
@@ -221,7 +251,7 @@ fun File.parse(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: 
                     r.forEachIndexed { x, p ->
 //                        if ( p == 0 ) return@forEachIndexed
 //                    channels[p].add(Pair(x, y))
-                       out.println("$x,$y: (${cga[p]})")
+                       out.println("$x,$y: (${palettes[bitsPerPixel]!![p]})")
                     }
                 }.toList()
             }
@@ -246,6 +276,26 @@ fun File.parse(mark: ByteArray, bitsPerPixel: Int = 2, width: Int = 12, height: 
         }
     }
     println(p)
+}
+
+fun File.export(offset: Int, count: Int, bitsPerPixel: Int = 2, width: Int = 12, height: Int = 11) {
+    val bytesPerRow = 8.lcm( bitsPerPixel * width ) / 8
+
+    for( i in 0 until count) {
+        val reader = inputStream()
+        reader.skip((offset + i * bytesPerRow * height).toLong())
+
+        val t = "${offset.toString(16)}.${(i+1).toString(10).padStart(3, '0')}"
+        val f = File("$t.txt")
+
+        f.printWriter().use {
+            val exporter = SpritePrinter.ImageMagickExporter(it, width, height,palettes[bitsPerPixel]!!)
+            reader.printSprite(
+                exporter, bitsPerPixel, width, height
+            )
+        }
+        execAndOut("convert -transparent black txt:$t.txt -define png:color-type=3 -define png:bit-depth=$bitsPerPixel -quality 100 $t.png")
+    }
 }
 // stdout catch
 fun execAndOut(command:String): String {
@@ -272,7 +322,7 @@ sealed class Term {
             execAndOut("/usr/bin/stty -F /dev/tty -echo")
         }
         override fun restore() { execAndOut("$TTY echo") }
-        override fun clear() { println("\u001b[H\u001b[2J") }
+        override fun clear() { print("\u001b[H\u001b[2J") }
         override val width get() = execAndOut("$TTY size").trim().split(" ")[1].toInt()
         override val height get() = execAndOut("$TTY size").trim().split(" ")[0].toInt()
         override fun withColor(s: String, c: Int) = "\u001b[38;5;${c % 255}m$s\u001B[0m"
@@ -281,30 +331,15 @@ sealed class Term {
     }
 }
 
-class AppContext {
-    var offset = 0 // live var
-    var bitsPerPixel = 4
-    var rowWidth = 32
-    var limitRows = Int.MAX_VALUE // but how many for export? to take snapshots....
-    // ruler
-    var skipEmptyRows = false
-    var export = false
-    var exit = false
-}
-
-const val ESC = 0x1b
-const val CSI = 0x5b
-const val CSI_CURSOR_UP = 'A'.code
-const val CSI_CURSOR_DOWN = 'B'.code
-const val CSI_CURSOR_FORWARD = 'C'.code
-const val CSI_CURSOR_BACK = 'D'.code
-const val KEY_PLUS = '+'.code
-const val KEY_MINUS = '-'.code
-const val KEY_SLASH = '/'.code
-const val KEY_ASTERIX = '*'.code
-
 enum class BitsPerPixel(val bits: Int) {
-    ONE(1), TWO(2), FOUR(4), EIGHT(8)
+    ONE(1), TWO(2), THREE(3),  FOUR(4), SIX(6), EIGHT(8),
+    NINE(9),
+    TWELVE(12),
+    FIFTEEN(15),
+    SIXTEEN(16),
+    EIGHTEEN(18),
+    RGB24(24),
+    RGB30(30)
     // full color? TBD
 }
 
@@ -347,11 +382,11 @@ sealed class Command( val match: List<Int>, private val command: AppContext.() -
     object WidthMore : Command(KEY_PLUS, {rowWidth += 1})
     class PageUp(private val term: Term) : Command(listOf(ESC) + "[5~".map { it.code }, {
         val rowsPass = min( limitRows, term.height - 2 )
-        this.offset -= rowsPass * this.rowWidth * bitsPerPixel / 8
+        this.offset -= calcColCount * rowsPass * this.rowWidth * bitsPerPixel / 8
     })
     class PageDown(private val term: Term) : Command(listOf(ESC) + "[6~".map { it.code }, {
         val rowsPass = min( limitRows, term.height - 2 )
-        this.offset += rowsPass * this.rowWidth * bitsPerPixel / 8
+        this.offset += calcColCount * rowsPass * this.rowWidth * bitsPerPixel / 8
     })
     object ScrollStart : Command(listOf(ESC) + "[H~".map { it.code }, { offset = 0 })
     class ScrollEnd(term: Term) : Command(listOf(ESC) + "[F~".map { it.code }, {
@@ -361,8 +396,6 @@ sealed class Command( val match: List<Int>, private val command: AppContext.() -
     object Exit : Command(listOf(ESC), command = { this.exit = true })
 }
 
-const val lblColor = 14
-const val valColor = 2
 fun Term.labeledValue(l: String, v: String) {
     print(withColor(" $l: ", lblColor))
     print(withColor(v, valColor))
@@ -370,16 +403,16 @@ fun Term.labeledValue(l: String, v: String) {
 
 fun redraw(term: Term, opts: AppContext, f: File) {
 
-    //validate
     val fl = f.length().toInt() // hope files not too much
     val h = term.height
-    val w = term.width
+    opts.termWidth = term.width
+    val w = opts.termWidth
 
     if ( opts.offset >= fl ) opts.offset -= fl
     if ( opts.offset < 0 ) opts.offset += fl
 
-    if ( opts.rowWidth < 0 ) opts.rowWidth += w
-    if ( opts.rowWidth >= fl ) opts.rowWidth -= fl
+    if ( opts.rowWidth < 1 ) opts.rowWidth = 1
+    if ( opts.rowWidth > fl ) opts.rowWidth = fl
 
     with(term) {
         clear()
@@ -395,50 +428,74 @@ fun redraw(term: Term, opts: AppContext, f: File) {
     val maxRows = min( opts.limitRows, h - headerHeight - footerHeight )
     val reader = f.inputStream()
     reader.skip(opts.offset.toLong())
-    val cols = (w + columnsPad) / (opts.rowWidth + columnsPad)
+    val cols = opts.calcColCount
+
 //println(f.readBytes().joinToString(", ") { it.toUByte().toString(2).padStart(8, '0') } )
     var redPixels = 0
     for ( c in 0 until cols ) {
         redPixels += reader.printSprite(
-            opts.bitsPerPixel, opts.rowWidth, maxRows, opts.skipEmptyRows, opts.export, term, c * (opts.rowWidth + columnsPad)
+            SpritePrinter.TermPrinter(term, opts.skipEmptyRows, c * (opts.rowWidth + columnsPad)),
+            opts.bitsPerPixel, opts.rowWidth, maxRows
         )
     }
 
-    val labeledHelp = fun(keys: String, lbl: String): Unit {
+    val labeledHelp = fun(keys: String, lbl: String) {
         print( term.withColor( term.withBackgroundColor("$keys", 7), 0) )
         print( term.withColor(" $lbl ", 7) )
     }
+    term.move(0, h - footerHeight)
     labeledHelp("ARROWS", "move offset by +-1 col or row")
     labeledHelp("/ *", "change bits per pixel")
     labeledHelp("- +", "change row width")
     labeledHelp("PGUP PGDN", "move offset by -1 page, +1 page")
     labeledHelp("HOME END", "move offset to start or end")
     labeledHelp("ESC", "exit")
+    term.move(0, 0)
 }
 
-@kotlin.ExperimentalUnsignedTypes
+@ExperimentalUnsignedTypes
 fun main(args: Array<String>) {
-    println(args.joinToString(", "))
+//    println(args.joinToString(", "))
+
     if (args.isEmpty()) {
         println("Usage: pixelHunter <file> <sprite width> <bit per pixel> <offset> <limit rows> <skip empty rows> <export>")
         exitProcess(-1)
     }
 
-    val f = File(args[0].toString())
+    if ( args[0] == "test") {
+        tests()
+        exitProcess(0)
+    }
+
+    val f = File(args[0])
 
     // batch mode
-    if ( args.size > 1 && args[1] == "mark" ) {
-        val mark = ByteArray(2) { listOf(0x0b, 0x03)[it].toByte() }
-        //check
-        f.parse(mark, 2, 12, 11)
-        exitProcess(0)
-//
-//        val b = f.readBytes()
-//        val c = b.filterIndexed { i, c -> c == 0x03.toByte() && b[i-1] == 0x0b.toByte() }.count()
-//        println(c)
-//
-//        reader.readAll(mark)
-//        exitProcess(0)
+    if ( args.size > 1 ) {
+        when(args[1]) {
+            "mark" -> {
+                val mark = ByteArray(2) { listOf(0x0b, 0x03)[it].toByte() }
+                //check
+                f.dumpAppleImages(mark, 2, 12, 11)
+                exitProcess(0)
+            }
+            "export" -> {
+                try {
+                    f.export(
+                        Integer.decode(args[2]),
+                        Integer.decode(args[3]),
+                        Integer.decode(args[4]),
+                        Integer.decode(args[5]),
+                        Integer.decode(args[6]),
+                    )
+                    exitProcess(0)
+                } catch (e: Throwable) {
+                    println("Error: $e")
+                    println("Usage: pixelHunter export <offset> <count of sprites> <bitsPerPixel> <sprite width> <sprite height>")
+                    e.printStackTrace()
+                    exitProcess(-1)
+                }
+            }
+        }
     }
 
     val opts = AppContext().apply {
@@ -463,7 +520,7 @@ fun main(args: Array<String>) {
     )
 
     var redraw = true
-    java.lang.System.console()?.reader()?.run {
+    System.console()?.reader()?.run {
         while( !opts.exit ) {
             if ( redraw ) { redraw( term, opts, f ); redraw = false }
 
@@ -485,3 +542,40 @@ fun main(args: Array<String>) {
     exitProcess(0)
 //    return 0
 }
+
+@OptIn(ExperimentalUnsignedTypes::class)
+fun tests() {
+
+    fun UByteArray.inputStream() = String(toByteArray(), Charsets.ISO_8859_1).byteInputStream(Charsets.ISO_8859_1)
+//    fun print(vararg a: UByte) = ubyteArrayOf(*a).inputStream().
+    val term = Term.Posix()
+
+    ubyteArrayOf(0x5du, 0x00u).inputStream().printSprite(SpritePrinter.TermPrinter(term), 1, 3, 3)
+    println()
+    ubyteArrayOf(0x1bu, 0x6fu, 0xbcu, 0xf0u).inputStream().printSprite(SpritePrinter.TermPrinter(term), 2, 4, 4)
+    println()
+    ubyteArrayOf(0x01u, 0x23u, 0x45u, 0x67u, 0x89u, 0xabu, 0xcdu, 0xefu).inputStream().printSprite(SpritePrinter.TermPrinter(term),
+        4, 4, 4
+    )
+    println()
+
+    val d = ubyteArrayOf(
+        0x24u, 0x92u, 0x49u,
+        0x60u, 0x00u, 0x03u,
+        0x64u, 0x12u, 0x4bu,
+        0xe3u, 0x8eu, 0x38u,
+        0x1cu, 0x71u, 0xc7u)
+
+    d.inputStream().printSprite(SpritePrinter.TermPrinter(term), 3, 8, 5)
+    println()
+}
+const val ESC = 0x1b
+const val CSI = 0x5b
+const val CSI_CURSOR_UP = 'A'.code
+const val CSI_CURSOR_DOWN = 'B'.code
+const val CSI_CURSOR_FORWARD = 'C'.code
+const val CSI_CURSOR_BACK = 'D'.code
+const val KEY_PLUS = '+'.code
+const val KEY_MINUS = '-'.code
+const val KEY_SLASH = '/'.code
+const val KEY_ASTERIX = '*'.code
