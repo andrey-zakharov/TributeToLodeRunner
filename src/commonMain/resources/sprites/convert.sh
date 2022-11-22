45a4fb4214550dfdc04ab7964d92e25bb2dffe5c
Andrey Zakharov <andrey.al.zakharov@gmail.com>
2022-11-22 15:19:20 +0300

    test

--- src/commonMain/resources/sprites/convert.sh
+++ src/commonMain/resources/sprites/convert.sh
@@ -1,8 +1,8 @@
-
+#!/usr/bin/env sh
 mkdir -p tiles
-cd tiles
+cd tiles || exit
 convert ../tiles.png -crop '23x25' +repage temptiles.%02d.png
-for f in temptiles.*.png; do if [ `identify -format '%w' "$f"` -lt 23 ]; then rm $f; fi; done
+for f in temptiles.*.png; do if [ "$(identify -format '%w' "$f")" -lt 23 ]; then rm $f; fi; done
 convert temptiles.*.png -crop 20x22+0+0 +repage tiles.png
 convert ../hole.png -crop '20x22' +repage hole.%02d.png
 rm hole.{05,06,07,08,16,23,24,25,34,35}.png #empty tiles
--- src/jvmMain/run.sh
+++ src/jvmMain/run.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env sh
 #java -cp pixelHunter.jar PixelHunter
-java -cp pixelHunter.jar:~/.sdkman/candidates/kotlin/1.7.20/lib/kotlin-stdlib.jar me.az.PixelHunterKt $*
+java -cp pixelHunter.jar:~/.sdkman/candidates/kotlin/1.7.20/lib/kotlin-stdlib.jar me.az.PixelHunterKt "$*"
